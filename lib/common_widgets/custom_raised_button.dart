22fe5a893c9cb50448297f18e0b7609a02fda52d
Peh Jun Siang <austin9090@hotmail.com>
2020-05-11 23:04:29 +0800

    changed padding to height

--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -1,32 +1,39 @@
 /*
  * Create a custom Raised button
  */
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomRaisedButton extends StatelessWidget {
-
   final Widget child;
   final Color color;
   final double borderRadius;
   final VoidCallback onPressed;
-  final double padding;
+  final double height;
 
-  CustomRaisedButton({this.child, this.color, this.padding, this.borderRadius, this.onPressed});
+  // custom raised button takes in the child, the height of the button,
+  // border radius for the button and callback
+  CustomRaisedButton(
+      {this.child,
+      this.height: 50,
+      this.color,
+      this.borderRadius: 3,
+      this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: this.child,
-      color: this.color,
-      padding: EdgeInsets.all(padding),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(this.borderRadius),
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        child: this.child,
+        color: this.color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(this.borderRadius),
+          ),
         ),
+        onPressed: this.onPressed,
       ),
-      onPressed: this.onPressed,
     );
   }
 }
