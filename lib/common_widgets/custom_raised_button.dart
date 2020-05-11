f90dd05a9f791653b728e17e974717f52e82a7f0
Peh Jun Siang <austin9090@hotmail.com>
2020-05-11 22:43:40 +0800

    add update padding feature custom raised button

--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -2,6 +2,7 @@
  * Create a custom Raised button
  */
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomRaisedButton extends StatelessWidget {
@@ -10,14 +11,16 @@ class CustomRaisedButton extends StatelessWidget {
   final Color color;
   final double borderRadius;
   final VoidCallback onPressed;
+  final double padding;
 
-  CustomRaisedButton({this.child, this.color, this.borderRadius, this.onPressed});
+  CustomRaisedButton({this.child, this.color, this.padding, this.borderRadius, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
       child: this.child,
       color: this.color,
+      padding: EdgeInsets.all(padding),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(
           Radius.circular(this.borderRadius),
