3cc64bb2930aa86f91880651754ebde47645244d
Andre Wong <wongandre98@gmail.com>
2020-05-14 20:16:52 +0800

    added named routes and different route names

---
+++ lib/Undefined/undefined.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Undefined extends StatelessWidget {
+  final String name;
+
+  const Undefined({Key key, this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Route for $name is not defined'),
+      ),
+    );
+  }
+}
